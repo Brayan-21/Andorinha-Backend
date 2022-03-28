b3ac8ff2661f6428e7688f67568482f84259b75f
Brayan <Brayan@Brayan>
2022-03-28 16:24:30 -0300

    Atualização sem jpa

--- backend/src/test/java/alternative/AlternativeProducers.java
+++ backend/src/test/java/alternative/AlternativeProducers.java
@@ -1,11 +1,7 @@
 package alternative;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import javax.annotation.Priority;
 import javax.enterprise.inject.Alternative;
-import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 
 @Alternative
 @Priority(1)
